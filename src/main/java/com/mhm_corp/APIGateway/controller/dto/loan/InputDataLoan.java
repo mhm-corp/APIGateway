b76f04a99da9bc4136683287bfed7502efa326d3
Helen Acero <hepacest@gmail.com>
2025-06-05 16:11:07 -0500

    The code to register a loan was updated.

--- src/main/java/com/mhm_corp/APIGateway/controller/dto/loan/InputDataLoan.java
+++ src/main/java/com/mhm_corp/APIGateway/controller/dto/loan/InputDataLoan.java
@@ -5,19 +5,21 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.math.BigDecimal;
+
 public record InputDataLoan(
         @NotNull(message = "Account number cannot be empty")
         String accountNumber,
         @NotNull(message = "Amount cannot be null")
         @Positive(message = "Amount must be greater than 0")
         @DecimalMin(value = "0.01", message = "Amount must be at least 0.01")
-        double amount,
+        BigDecimal amount,
         @NotNull(message = "Installments cannot be null")
         @Min(value = 1, message = "Minimum number of installments is 1")
         int installments,
         @NotNull(message = "Interest rate cannot be null")
         @DecimalMin(value = "0.0", inclusive = true, message = "Interest rate must be positive or zero")
-        double interestRate
+        BigDecimal effectiveAnnualInterestRate
 ) {
 
 }
